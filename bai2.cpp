4bb11fd15d01ed4fd37ea0273bb53335faf1926c
Truongduykhuong <truongduykhuong2010@gmail.com>
2018-11-28 19:01:56 +0700

    comment

---
+++ bai2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int cc;
+	cout<<"NHAP A :"<<endl;
+	cin>>a;
+	cout<<"NHAP B :"<<endl;
+	cin>>b;
+	bool c = a < b;
+	cout<<c;
+}
